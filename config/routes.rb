2a7502a7478fa3203c5495f29276e3383762d982
starrychloe <csowers@gcinfotech.com>
2013-02-24 01:31:56 -0500

    Ch 8. Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ RailsinstallerDemo::Application.routes.draw do
 
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
 
   get "welcome/index"
 
